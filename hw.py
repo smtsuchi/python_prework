9ae6d27f1a3c2bcb3c9ff6a7d93e7440a08b4413
smtsuchi <shoha1999@gmail.com>
2022-02-07 19:08:41 -0500

    first commit

---
+++ hw.py
@@ -0,0 +1,12 @@
+def is_consecutive(a_list):
+    i = 0
+    status = True
+    while i < len(a_list) - 1:
+       if a_list[i] + 1 == a_list[i + 1]:
+           i += 1
+       else:
+            status = False
+            break
+    print(status)
+
+is_consecutive([])
